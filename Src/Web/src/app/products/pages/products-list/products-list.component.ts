7eb75c4fa2eb67e231020359dbc35f8da239f1ab
Bianca <bianca.tizu97@yahoo.com>
2019-12-30 08:49:52 +0200

    Added request product component and different view for regular user

--- Src/Web/src/app/products/pages/products-list/products-list.component.ts
+++ Src/Web/src/app/products/pages/products-list/products-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { MatTableDataSource, MatDialog } from '@angular/material';
 import { AddProductComponent } from '../add-product/add-product.component';
+import { RequestProductComponent } from '../request-product/request-product.component';
 
 @Component({
   selector: 'app-products-list',
@@ -9,12 +10,13 @@ import { AddProductComponent } from '../add-product/add-product.component';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+  private isPharmacist: boolean = false;
   private mockData: Product[] = [
-    new Product('Diazepam',15.25,new Date('2020-01-15'), 100, 'Courier', 'On stock'),
-    new Product('Theraflu',2.5,new Date('2020-06-15'), 50, 'Courier', 'Out of stock'),
-    new Product('Fervex',1.5,new Date('2020-01-15'), 50, 'Personal pickup', 'Out of stock')
+    new Product('Diazepam', 15.25, new Date('2020-01-15'), 100, 'Courier', 'On stock'),
+    new Product('Theraflu', 2.5, new Date('2020-06-15'), 50, 'Courier', 'Out of stock'),
+    new Product('Fervex', 1.5, new Date('2020-01-15'), 50, 'Personal pickup', 'Out of stock')
   ];
-  private displayedColumns: string[] = ['name','description','price','expiryDate', 'quantity', 'deliveryOption', 'availability', 'actions']
+  private displayedColumns: string[] = ['name', 'description', 'price', 'expiryDate', 'quantity', 'deliveryOption', 'availability', 'actions']
   private dataSource = new MatTableDataSource<Product>(this.mockData);
   constructor(public dialog: MatDialog) { }
 
@@ -31,6 +33,16 @@ export class ProductsListComponent implements OnInit {
         isEdit: false
       }
     });
+  }
+
+  openRequestDialog(product: Product): void {
+    const dialogRef = this.dialog.open(RequestProductComponent, {
+
+    });
+  }
+
+  toggle(): void {
+    this.isPharmacist = !this.isPharmacist;
   }
 
 }
--- Src/Web/src/app/products/products.routing.module.ts
+++ Src/Web/src/app/products/products.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddProductComponent } from './pages/add-product/add-product.component';
 import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { RequestProductComponent } from './pages/request-product/request-product.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: 'add',
     pathMatch: 'full',
     component: AddProductComponent
+  },
+  {
+    path: 'request',
+    component: RequestProductComponent
   }];
 
 @NgModule({
