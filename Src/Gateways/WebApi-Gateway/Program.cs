cd4f76d66bc4c879dc84691ccb8167cf3ddd8de2
Ursaciuc Adrian <ursaciuc.adrian27@gmail.com>
2020-01-03 15:40:06 +0200

    Add other services

--- Src/Gateways/WebApi-Gateway/Program.cs
+++ Src/Gateways/WebApi-Gateway/Program.cs
@@ -27,44 +27,5 @@ namespace Medifii.ApiGateway
 				{
 					webBuilder.UseStartup<Startup>();
 				});
-
-		//public static void Main(string[] args)
-		//{
-		//	new WebHostBuilder()
-		//		.UseKestrel()
-		//		.UseContentRoot(Directory.GetCurrentDirectory())
-		//		.ConfigureAppConfiguration((hostingContext, config) =>
-		//		{
-		//			config
-		//				.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-		//				.AddJsonFile("appsettings.json", true, true)
-		//				.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-		//				.AddJsonFile("ocelot.json")
-		//				.AddEnvironmentVariables();
-		//		})
-		//		.ConfigureServices(s =>
-		//		{
-		//			s.AddCors(options =>
-		//			{
-		//				options.AddPolicy("CorsPolicy",
-		//					builder => builder.AllowAnyOrigin()
-		//						.AllowAnyMethod()
-		//						.AllowAnyHeader());
-		//			});
-		//			s.AddOcelot();
-		//		})
-		//		.ConfigureLogging((hostingContext, logging) =>
-		//		{
-		//			logging.AddConsole();
-		//		})
-		//		.UseIISIntegration()
-		//		.Configure(app =>
-		//		{
-		//			app.UseCors("CorsPolicy");
-		//			app.UseOcelot().Wait();
-		//		})
-		//		.Build()
-		//		.Run();
-		//}
 	}
 }
