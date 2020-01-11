3c163e33fe5439e8e8bed752c9aef5829e1238d3
SerbanMatei <48119059+SerbanMatei@users.noreply.github.com>
2020-01-11 13:11:11 +0200

    Integrated dbProducts with scraperProducts

--- Src/Microservices/Search-Service/Medifii.SearchService.Api/Queries/SearchProductsHandler.cs
+++ Src/Microservices/Search-Service/Medifii.SearchService.Api/Queries/SearchProductsHandler.cs
@@ -3,8 +3,10 @@ using Medifii.SearchService.Dto;
 using Medifii.SearchService.Interfaces;
 using Medifii.SearchService.Queries;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Medifii.SearchService.Api.Mappers;
 
 namespace Medifii.SearchService.Api.Queries
 {
@@ -22,7 +24,11 @@ namespace Medifii.SearchService.Api.Queries
 		public async Task<IEnumerable<ProductDto>> Handle(SearchProductsQuery request, CancellationToken cancellationToken)
         {
             var dbProducts = await _productClient.GetProducts(request.Name);
-			return await _scraperClient.GetProductsAsync(request.Name);
-		}
+			var scraperProducts =  await _scraperClient.GetProductsAsync(request.Name);
+            var dbProductsDto = dbProducts.ToProductDto();
+            var allProducts = dbProductsDto.ToList();
+            allProducts.AddRange(scraperProducts);
+            return allProducts;
+        }
 	}
 }
--- Src/Microservices/Search-Service/Medifii.SearchService/Interfaces/IProductClient.cs
+++ Src/Microservices/Search-Service/Medifii.SearchService/Interfaces/IProductClient.cs
@@ -8,6 +8,6 @@ namespace Medifii.SearchService.Interfaces
     public interface IProductClient
     {
         [Get("/api/product/{name}")]
-        Task<List<DbProductDto>> GetProducts([Path] string query);
+        Task<List<DbProductDto>> GetProducts([Path] string name);
     }
 }
