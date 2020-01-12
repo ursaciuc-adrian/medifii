94e4efc2d6bfd91796e0f4feb308ff0c1e1c1c30
SerbanMatei <48119059+SerbanMatei@users.noreply.github.com>
2020-01-12 12:12:30 +0200

    Added quantity to request

---
+++ Src/Microservices/Request-Service/Medifii.RequestService.DomainModel/Medifii.RequestService.Persistence/Migrations/20200112101020_AddQuantity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Medifii.RequestService.Persistence.Migrations
+{
+    public partial class AddQuantity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Requests",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Requests");
+        }
+    }
+}
