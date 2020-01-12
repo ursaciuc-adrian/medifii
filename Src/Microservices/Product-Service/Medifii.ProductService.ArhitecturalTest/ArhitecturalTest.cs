f620ad86c85ee24b7caf08183752be7783631132
SerbanMatei <48119059+SerbanMatei@users.noreply.github.com>
2020-01-12 15:44:01 +0200

    Add arhitecture tests

---
+++ Src/Microservices/Product-Service/Medifii.ProductService.ArhitecturalTest/ArhitecturalTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace Medifii.ProductService.ArhitecturalTests
+{
+    public class ArhitecturalTest
+    {
+        [Fact]
+        public void ControllersShouldNotDirectlyReferenceRepositories()
+        {
+            var result = Types.InCurrentDomain()
+                .That()
+                .ResideInNamespace("Medifii.ProductService.Controllers")
+                .ShouldNot()
+                .HaveDependencyOn("Medifii.ProductService.Data")
+                .GetResult();
+            result.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
