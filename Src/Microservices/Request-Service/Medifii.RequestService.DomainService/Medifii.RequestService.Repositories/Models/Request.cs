321505657e738d0ea05d151a1b0ae79396ae681c
SerbanMatei <48119059+SerbanMatei@users.noreply.github.com>
2020-01-12 23:17:41 +0200

    Added id to req model

--- Src/Microservices/Request-Service/Medifii.RequestService.DomainService/Medifii.RequestService.Repositories/Models/Request.cs
+++ Src/Microservices/Request-Service/Medifii.RequestService.DomainService/Medifii.RequestService.Repositories/Models/Request.cs
@@ -4,6 +4,8 @@ namespace Medifii.RequestService.Repositories.Models
 {
     public class Request
     {
+        public Guid Id { get; set; }
+
         public Guid PharmacyId { get; set; }
 
         public string ProductName { get; set; }
